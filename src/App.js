c8a77f8c3230e80940942b2ea06e75948468eef6
Owen Velkoff <owenv@lawn-143-215-48-179.lawn.gatech.edu>
2023-01-06 14:42:06 -0500

    barebones site

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+        <p> This is the new personal website of Owen Velkoff </p>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
