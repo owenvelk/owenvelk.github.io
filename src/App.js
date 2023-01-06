495d5114674da03047cade394aa9aab766a9c3c3
Owen Velkoff <owenv@lawn-143-215-48-179.lawn.gatech.edu>
2023-01-06 15:23:03 -0500

    hope this works

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p> */}
         <p> This is the new personal website of Owen Velkoff </p>
+        <p> it will be very cool </p>
         {/* <a
           className="App-link"
           href="https://reactjs.org"
