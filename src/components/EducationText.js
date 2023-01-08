d7402690d86775da92d775d19e4576e1e020e924
Owen Velkoff <owenv@lawn-143-215-48-179.lawn.gatech.edu>
2023-01-08 11:59:33 -0500

    changes to intro and education components

--- src/components/EducationText.js
+++ src/components/EducationText.js
@@ -3,7 +3,7 @@ import '../styles/Education.css';
 export default function EducationText() {
     return (
         <div className="EducationText">
-            <p styles="font-size: 50px">My Education</p>
+            <h3>My Education</h3>
             <p>I'm in my third year of studying Computer Science at Georgia Tech</p>
             <p>My threads (focuses) are in Intelligence and Information Internetworks</p>
             <p>I've worked as a Teaching Assistant for CS 1331, Georgia Tech's Intro to Object-Oriented Programming class</p>
--- src/components/IntroText.js
+++ src/components/IntroText.js
@@ -3,7 +3,7 @@ import '../styles/Intro.css';
 export default function IntroText () {
     return (
         <div className='IntroText'>
-            <p> Hi! My name is Owen Velkoff </p>
+            <h3> Hi! My name is Owen Velkoff </h3>
             <p> I'm a Computer Science student at Georgia Tech </p>
             <p> I'm interested in full-stack development, with plenty of experience in front-end </p>
         </div>
